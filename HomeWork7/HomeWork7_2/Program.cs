d1298552178779861257935c8db5ea1a7338c105
KAMO-PC\Admin <you@example.com>
2025-05-28 23:20:56 +0300

    HomeWork7

---
+++ HomeWork7/HomeWork7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeWork7_2;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[,] arr = new int[3,3];
+
+        for(var i = 0; i < arr.GetLength(0); i++)
+        {   
+            var maxValue = arr[i, 0];
+            for (var j = 0; j < arr.GetLength(1); j++)
+            {
+                arr[i, j] = rnd.Next(1, 100);
+
+                Console.Write($"{arr[i, j],4}");
+
+                if (arr[i, j] >  maxValue)
+                {
+                    maxValue = arr[i, j];   
+                }
+            }
+            Console.WriteLine($" Максимальное значение в строке {i + 1}: {maxValue}");
+        }
+    }
+}
