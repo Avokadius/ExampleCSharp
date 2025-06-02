04c95482c9d6ad1dd65a82593976673d0d0b821e
KAMO-PC\Admin <you@example.com>
2025-06-02 22:33:01 +0300

    add HomeWork8

---
+++ HomeWork8/HomeWork8/Students.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeWork8;
+public class Students
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double AverageScore { get; set; }
+    public  void Print()
+    {
+        Console.WriteLine($"Имя: {Name}\n Возраст: {Age}\n Средний балл: {AverageScore}");
+    }
+}
+
+
