ebcde81c5bf67859faab646e0f3ef71c308b6dce
KAMO-PC\Admin <you@example.com>
2025-06-11 19:26:50 +0300

    fixedObjectEx

--- ObjectEx1/ObjectEx/ObjectEx/ObjectEx/Models/Person.cs
+++ ObjectEx1/ObjectEx/ObjectEx/ObjectEx/Models/Person.cs
@@ -1,16 +1,12 @@
 ﻿namespace ObjectEx.Models;
-
 public class Person
 {
     public string Name { get; set; }
-
     public int Age { get; set; }
-
     public override string ToString()
     {
         return $"Name: {Name}, Age: {Age}";
     }
-
     public override bool Equals(object? obj)
     {
         if (obj == null)
@@ -30,13 +26,11 @@ public class Person
 
         return false;
     }
-
     // GetHashCode() - для видимости 
     public override int GetHashCode()
     {
         return HashCode.Combine(Name, Age);
     }
-
     public static bool operator ==(Person a, Person b)
     {
         if (ReferenceEquals(a, b))
@@ -51,11 +45,8 @@ public class Person
 
         return a.Equals(b);
     }
-
     public static bool operator !=(Person a, Person b)
     {
         return !(a == b);
     }
-
-
 }
