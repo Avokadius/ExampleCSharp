d0ce3b67c4d0d2474835b1ef5450857933836732
KAMO-PC\Admin <you@example.com>
2025-06-11 18:31:51 +0300

    AddHomeWork7

---
+++ HomeWork7/HomeWork7_3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HomeWork7_3._1;
+internal class Program
+{
+    static bool Palindrom(string str)
+    {
+        var deleteStr = str.Replace(" ", "").ToLower();
+
+        // Сравниваем строку с её обратной версией
+        char[] charArray = deleteStr.ToCharArray();
+        Array.Reverse(charArray);
+
+        var reversedStr = new string(charArray);
+
+        return deleteStr == reversedStr;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку для проверки является ли она палиндромом: ");
+        var input = Console.ReadLine();
+
+        if (Palindrom(input))
+        {
+            Console.WriteLine($"Слово {input} является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine($"Слово {input} не является палиндромом.");
+        }
+
+    }
+}
