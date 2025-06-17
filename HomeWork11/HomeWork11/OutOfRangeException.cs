875f5df9ad7c35b6e66c24feeba5a7fc599b7155
KAMO-PC\Admin <you@example.com>
2025-06-17 23:46:04 +0300

    HomeWork11

---
+++ HomeWork11/HomeWork11/OutOfRangeException.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork11;
+public class OutOfRangeException : Exception
+{
+    public OutOfRangeException() : base ("число выходит за предел диапазона!")
+    {
+    }
+    public OutOfRangeException(string message) : base(message)
+    {
+    }
+    public OutOfRangeException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
+
