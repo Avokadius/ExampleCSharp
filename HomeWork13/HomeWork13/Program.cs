85a68c218374dd72f960309935dc546c4b4719ec
KAMO-PC\Admin <you@example.com>
2025-06-24 23:59:25 +0300

    HomeWork13

---
+++ HomeWork13/HomeWork13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata;
+namespace HomeWork13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewsProvider newsProvider = new NewsProvider();
+
+            Client lyci = new Client("Люси");
+            Client alla = new Client("Алла");
+            Client bogdan = new Client("Богдан");
+
+            newsProvider.Subscribe(lyci, "Новости");
+            newsProvider.Subscribe(alla, "Спорт");
+            newsProvider.Subscribe(bogdan, "Погода");
+            newsProvider.Subscribe(bogdan, "Новости");
+            newsProvider.Subscribe(lyci, "Спорт");
+
+            newsProvider.Notify("Новости", "Сегодня важные события в мире.");
+            newsProvider.Notify("Спорт", "Команда выиграла матч!");
+            newsProvider.Notify("Погода", "Завтра ожидается дождь.");
+
+            newsProvider.Unsubscribe(alla, "Спорт");
+            newsProvider.Notify("Спорт", "Новые результаты чемпионата.");
+        }
+    }
+}
