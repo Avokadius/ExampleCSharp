8fcca7c0e8f90f208024826131128f762ce6e333
KAMO-PC\Admin <you@example.com>
2025-06-26 23:30:38 +0300

    HomeWork12

---
+++ HomeWork12/HomeWork12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager userManager = new UserManager();
+            userManager.AddUser(new User { Name = "Denis", Age = 41, Email = "denis@example.com" });
+            userManager.AddUser(new User { Name = "Alex", Age = 33, Email = "alex@example.com" });
+            userManager.AddUser(new User { Name = "Maria", Age = 29, Email = "maria@example.com" });
+
+            userManager.SaveUsers();
+
+            Console.WriteLine("Список пользователей:");
+            userManager.ShowUsers();
+
+            // Завершение программы
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
