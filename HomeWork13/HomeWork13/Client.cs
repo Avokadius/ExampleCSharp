7234b6c7249ac6d90ad262fe1e1e12d648aae890
KAMO-PC\Admin <you@example.com>
2025-07-08 00:00:43 +0300

    ReRemakeHomeWork13

--- HomeWork13/HomeWork13/Client.cs
+++ HomeWork13/HomeWork13/Client.cs
@@ -2,14 +2,13 @@
 public class Client
 {
     private readonly string name;
-    
     public Client(string name)
     {
         this.name = name;
     }
-
     public void ReceiveNews(string message)
     {
         Console.WriteLine($"{name} получил новость: {message}");
     }
 }
+
